64d5a06fb214422c6506a96c60cc9ac260a9c9ca
Summiyaashraf <168761262+Summiyaashraf@users.noreply.github.com>
2024-09-25 22:06:58 +0500

    Update script.ts

--- milestone 1/script.ts
+++ milestone 1/script.ts
@@ -1,10 +1,12 @@
-const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement
-const skills =document.getElementById('skills') as HTMLElement
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement;
+    const skills = document.getElementById('skills') as HTMLElement;
 
-toggleButton.addEventListener('click' , ()=> {
-    if(skills.style.display === 'none') {
-        skills.style.display = 'block'
-    } else {
-        skills.style.display = 'none'
-    }
+    toggleButton.addEventListener('click', () => {
+        if (skills.style.display === 'none') {
+            skills.style.display = 'block';
+        } else {
+            skills.style.display = 'none';
+        }
+    });
 });
